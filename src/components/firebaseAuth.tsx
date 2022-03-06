d2eb82169953bcbc39c34bb2c663eca82da78a6c
Goran <pavlovskigoran88@gmail.com>
2022-03-06 02:32:05 +0100

    implement auth login page and redirect if they are logged in

--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,35 @@
 // import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import "firebase/auth";
+import { EmailAuthProvider, getAuth } from "@firebase/auth";
+import firebaseui from "firebaseui";
+import { FunctionComponent, useEffect, useState } from "react";
 
-export {};
+const firebaseAuthConfig: firebaseui.auth.Config = {
+  signInFlow: "popup",
+  signInOptions: [
+    {
+      provider: EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  signInSuccessUrl: "/",
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+  const [renderAuth, setRenderAuth] = useState(false);
+  useEffect(() => {
+    setRenderAuth(true);
+  }, []);
+
+  const auth = getAuth();
+  return (
+    <div className="mt-16">
+      {renderAuth ? (
+        <StyledFirebaseAuth uiConfig={firebaseAuthConfig} firebaseAuth={auth} />
+      ) : null}
+    </div>
+  );
+};
+
+export default FirebaseAuth;
